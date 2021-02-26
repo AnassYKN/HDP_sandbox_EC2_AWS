0f1863c03a05eaf77332ecaeb006cdc601ea741b
Anass YAKINE <47039179+AnassYKN@users.noreply.github.com>
2021-02-26 12:25:16 +0100

    add some modifications on script docker install

--- install_docker.sh
+++ install_docker.sh
@@ -3,17 +3,30 @@
 ## Reference https://docs.aws.amazon.com/AmazonECS/latest/developerguide/docker-basics.html
 
 ## install docker
+
+```
 sudo apt-get update 
 sudo apt-get install docker.io
+```
 
 ## install git
+
+```
 sudo apt-get install git
+```
 
 ## Check version docker
+```
 docker version
+```
 
 ## add permission to docker
+```
 sudo chmod 666 /var/run/docker.sock
+```
 
 ## start docker
+
+```
 sudo service docker start
+```
